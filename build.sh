01ad72bde0ec093c2de9ec519e3a3b916a8c1b43
joshuaspiral <82312141+joshuaspiral@users.noreply.github.com>
2022-03-16 08:09:21 +0800

    -O3 optimisation

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 set -xe
-cc -Wall -Wextra -ggdb -o twoplayer twoplayer.c -lm
-cc -Wall -Wextra -ggdb -o ai ai.c -lm
-cc -Wall -Wextra -ggdb -o ai_vs_ai ai_vs_ai.c -lm
+cc -Wall -Wextra -ggdb -O3 -o twoplayer twoplayer.c -lm
+cc -Wall -Wextra -ggdb -O3 -o ai ai.c -lm
+cc -Wall -Wextra -ggdb -O3 -o ai_vs_ai ai_vs_ai.c -lm
